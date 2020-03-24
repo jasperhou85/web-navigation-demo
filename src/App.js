e20fc2926986e29855aacba64a532c62c57629b0
Jasper Hou <jasper.hou@bmw.com>
2020-03-24 12:10:33 +0800

    add the map key

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { Map, Marker } from 'react-amap';
 function App() {
   return (
       <div style={{ width: '100%', height: '100%', position: 'fixed' }}>
-          <Map>
-            
+          <Map amapkey={"f969d73361ac79af3ca61ac140a75719"} zoom={16}>
+              <Marker />
           </Map>
       </div>
   );

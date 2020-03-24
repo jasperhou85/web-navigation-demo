a75f2a266bf48d9543ef2ff02ee3d4c65bb69c6c
Jasper Hou <jasper.hou@bmw.com>
2020-03-24 10:19:02 +0800

    Updates

---
+++ precache-manifest.ad6649e2c6fb6fbb920952f14ebc7753.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2908844e0a97d38b2b6a6c69e9caa8",
+    "url": "/web-navigation-demo/index.html"
+  },
+  {
+    "revision": "eb771f85e6c225a98285",
+    "url": "/web-navigation-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "08888dfd74ef72fe1cce",
+    "url": "/web-navigation-demo/static/js/2.70055db7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/web-navigation-demo/static/js/2.70055db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb771f85e6c225a98285",
+    "url": "/web-navigation-demo/static/js/main.5a0283a1.chunk.js"
+  },
+  {
+    "revision": "5018239b1b4307ac7969",
+    "url": "/web-navigation-demo/static/js/runtime-main.ba24e976.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web-navigation-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
